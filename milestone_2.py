4e380f368eed4fb4a4d01cc84a10b4554347ba0a
JoeWilson24 <joewilson044@gmail.com>
2024-11-19 10:25:09 +0100

    Adds completed changes to milestone_2.py and README.md

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,16 @@
+import random
+
 word_list = ["banana", "strawberry", "peach", "mango", "melon"]
 
 print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input("Please enter a single letter guess:")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops, that is not a valid input")
