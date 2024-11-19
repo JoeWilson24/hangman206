1ea80b300d2be2353dec8c7cd2299235de3c05c3
JoeWilson24 <joewilson044@gmail.com>
2024-11-19 14:37:25 +0100

    Adds ask_for_input and check_guess functions to milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,22 @@
 #random fruit imported
 from milestone_2 import word
 
-#prompts user to input a letter and checks whether the format is correct
-while True: 
-    guess = input("Please enter a single letter guess:")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character")
+#Function to prompt user to input a letter and checks whether the format is correct
+def ask_for_input():
+    while True: 
+        guess = input("Please enter a single letter guess:")
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character")
+    check_guess(guess)
 
-#Check whether guessed letter is in word
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+#Function to convert guessed letter to lower case check whether guessed letter is in word
+def check_guess(guess):
+    lower_guess = (guess.lower())
+    if lower_guess in word:
+        print(f"Good guess! {lower_guess} is in the word.")
+    else:
+        print(f"Sorry, {lower_guess} is not in the word. Try again.")
+
+ask_for_input()
